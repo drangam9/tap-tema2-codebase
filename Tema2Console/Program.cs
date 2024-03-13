ee1e9ee5c16647b50fc013bb78407d26491a7461
Rusu, Iulian <iulian.rusu@centric.eu>
2024-03-14 00:50:15 +0200

    Add project files.

---
+++ Tema2Console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Tema2Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client...");
+
+            var hotelReception = new HotelReception();
+            hotelReception.ProcessOrder();
+
+            if (hotelReception.FinalPrice == 0)
+            {
+                Console.WriteLine("No order was processed.");
+            }
+            else
+            {
+                Console.WriteLine($"Final price for you order: {hotelReception.FinalPrice} RON");
+            }
+        }
+    }
+}
